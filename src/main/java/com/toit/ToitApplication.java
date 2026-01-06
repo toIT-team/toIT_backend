793ae43e5aea8b27bae17a09e913bb5e20e9b09a
whitecy01 <whitecy01@naver.com>
2026-01-06 23:38:23 +0900

    프로젝트 환경 구성

---
+++ src/main/java/com/toit/ToitApplication.java
@@ -0,0 +1,13 @@
+package com.toit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toit/ToitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
