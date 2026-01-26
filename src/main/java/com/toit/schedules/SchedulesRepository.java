727a3bd197b4960e42ac63ec908eb5758788534a
철딩 <cheolding2168@gmail.com>
2026-01-26 17:58:36 +0900

    feat(schedules): schedules 컨트롤러, 서비스, 레포지토리 추가 - 메인 페이지에서 조회할 오늘의 스케줄 정보 api 구축중 Refs: #3

--- src/main/java/com/toit/schedules/SchedulesRepository.java
+++ src/main/java/com/toit/schedules/SchedulesRepository.java
@@ -4,7 +4,10 @@ import com.toit.schedules.Schedules;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SchedulesRepository extends JpaRepository<Schedules, Long> {
+
 
 }
