53a3d34c8df48ee8f01800fb10ec22c76febcd3d
whitecy01 <whitecy01@naver.com>
2026-01-26 20:57:28 +0900

    feat(folders): 서비스, 컨트롤러 추가 Refs: #20

---
+++ src/main/java/com/toit/folders/FoldersController.java
@@ -0,0 +1,7 @@
+package com.toit.folders;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FoldersController {
+}
---
+++ src/main/java/com/toit/folders/FoldersService.java
@@ -0,0 +1,7 @@
+package com.toit.folders;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FoldersService {
+}
