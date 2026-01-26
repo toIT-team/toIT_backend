9023243ddf12a16eabb5f582c9da045d73b34c71
whitecy01 <whitecy01@naver.com>
2026-01-26 20:56:06 +0900

    feat(folders, users): 다대일 연관관계 단방향으로 변경 - 양방향으로 되어있던 걸 단방향으로 변경 Refs: #20

--- src/main/java/com/toit/user/Users.java
+++ src/main/java/com/toit/user/Users.java
@@ -81,13 +81,6 @@ public class Users {
      * 외래 키의 주인은 Folders
      */
     @OneToMany(mappedBy = "users")
-    private List<Folders> folders = new ArrayList<>();
-
-    /**
-     * 보관함(folders)와 1:N 관계
-     * 외래 키의 주인은 Folders
-     */
-    @OneToMany(mappedBy = "users")
     private List<Folders> items = new ArrayList<>();
 
 }
