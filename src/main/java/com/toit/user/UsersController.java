04abff15b9196633decff573625f5ba14efb349e
whitecy01 <whitecy01@naver.com>
2026-01-29 15:13:22 +0900

    feat(users): users 생성 API 추가
    Refs: #26

--- src/main/java/com/toit/user/UsersController.java
+++ src/main/java/com/toit/user/UsersController.java
@@ -1,6 +1,11 @@
 package com.toit.user;
 
+import com.toit.user.dto.request.UsersCreateRequest;
+import com.toit.user.dto.response.UsersCreateResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +17,11 @@ public class UsersController {
     private final UsersService usersService;
 
     /** 사용자 생성 - 카카오 로그인 전 임시 */
-
+    @PostMapping
+    public ResponseEntity<UsersCreateResponse> createUser(
+            @RequestBody UsersCreateRequest request
+    ) {
+        return ResponseEntity.ok(usersService.createUser(request));
+    }
 
 }
--- src/main/java/com/toit/user/dto/response/UsersCreateResponse.java
+++ src/main/java/com/toit/user/dto/response/UsersCreateResponse.java
@@ -1,5 +1,6 @@
 package com.toit.user.dto.response;
 
+import com.toit.common.enums.AuthProvider;
 import com.toit.user.Users;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
@@ -13,12 +14,16 @@ public class UsersCreateResponse {
     private Long usersId;
     private String email;
     private String name;
+    private AuthProvider authProvider;
+    private Long providerUsersId;
     private LocalDateTime createdAt;
 
     public UsersCreateResponse(Users users) {
         this.usersId = users.getUsersId();
         this.email = users.getEmail();
         this.name = users.getName();
+        this.authProvider = users.getAuthProvider();
+        this.providerUsersId = users.getProviderUsersId();
         this.createdAt = users.getCreatedAt();
     }
 }
