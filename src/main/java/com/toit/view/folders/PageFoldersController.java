2e7391424ea5d6cee0314a460d0c9cfe13545822
whitecy01 <whitecy01@naver.com>
2026-02-06 21:30:40 +0900

    feat(page-folders, page-home): Swagger Tag 이름 변경
    Refs: #62

--- src/main/java/com/toit/view/folders/PageFoldersController.java
+++ src/main/java/com/toit/view/folders/PageFoldersController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Pages - Home", description = "Home 화면(page) 전용 API")
+@Tag(name = "Pages - Folders", description = "Home 화면(page) 전용 API")
 @RestController
 @RequestMapping("/page/folders")
 @RequiredArgsConstructor
--- src/main/java/com/toit/view/home/PageHomeViewController.java
+++ src/main/java/com/toit/view/home/PageHomeViewController.java
@@ -5,12 +5,12 @@ import com.toit.view.home.dto.response.PageHomeViewResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Pages - Folders", description = "보관함 화면(page) 전용 API")
+@Tag(name = "Pages - Home", description = "보관함 화면(page) 전용 API")
 @RestController
 @RequestMapping("/page/home")
 @RequiredArgsConstructor
@@ -18,7 +18,7 @@ public class PageHomeViewController {
 
     private final PageHomeUseCase pageHomeUseCase;
 
-    @GetMapping
+    @PostMapping
     public ResponseEntity<PageHomeViewResponse> getHome(
             @RequestBody PageHomeViewRequest request
     ) {
