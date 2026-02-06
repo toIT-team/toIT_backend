e3601f138548e0221b42116035a1a1061f2a5ff2
whitecy01 <whitecy01@naver.com>
2026-02-06 20:52:46 +0900

    feat(page-folders, page-home): /page URL 추가
    Refs: #62

--- src/main/java/com/toit/view/folders/FoldersViewController.java
+++ src/main/java/com/toit/view/folders/FoldersViewController.java
@@ -1,4 +1,11 @@
 package com.toit.view.folders;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/page/folders")
+@RequiredArgsConstructor
 public class FoldersViewController {
 }
