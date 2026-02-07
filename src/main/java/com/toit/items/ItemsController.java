e21344c9f958a0fbc8ed389e9d017efdcd275324
whitecy01 <whitecy01@naver.com>
2026-02-07 22:47:42 +0900

    feat(items): TEXT 자료 생성 POST 추가
    Refs: #73

---
+++ src/main/java/com/toit/items/ItemsController.java
@@ -0,0 +1,32 @@
+package com.toit.items;
+
+import com.toit.items.dto.request.ItemsTextCreateReqeust;
+import com.toit.items.dto.response.ItemsTextCreateResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/items")
+@RequiredArgsConstructor
+public class ItemsController {
+    private final ItemsService itemsService;
+
+    /**
+     * <h2>itesm의 text 추가 컨트롤러</h2>
+     */
+    @Operation(
+            summary = "자료 텍스트 추가 API - 화면이름 : 자료-링크저장",
+            description = "자료 추가는 POST입니다."
+    )
+    @PostMapping("/text")
+    public ResponseEntity<ItemsTextCreateResponse> createFoldersText(
+            @RequestBody ItemsTextCreateReqeust request
+    ){
+        return ResponseEntity.ok(itemsService.createFoldersText(request.getUsersId(), request.getFoldersIdList(), request.getName(), request.getTextContent()));
+    }
+}
