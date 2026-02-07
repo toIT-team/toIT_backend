c6ed821b0aa292c580dc6baf79ea0e1ea97aa2ab
whitecy01 <whitecy01@naver.com>
2026-02-07 19:23:08 +0900

    feat(page-folders): POST에서 GET으로 변경
    Refs: #62

--- src/main/java/com/toit/view/folders/PageFoldersController.java
+++ src/main/java/com/toit/view/folders/PageFoldersController.java
@@ -1,13 +1,12 @@
 package com.toit.view.folders;
 
-import com.toit.view.folders.dto.request.PageFoldersMemoRequest;
 import com.toit.view.folders.dto.response.PageFoldersMemoResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Pages - Folders", description = "Home 화면(page) 전용 API")
@@ -17,10 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class PageFoldersController {
 
     private final PageFoldersUseCase pageFoldersUseCase;
-    @PostMapping("/memo")
+
+
+    @GetMapping("/memo")
     public ResponseEntity<PageFoldersMemoResponse> getOneFoldersMemo(
-            @RequestBody PageFoldersMemoRequest request
+            @RequestParam("usersId") Long usersId,
+            @RequestParam("foldersId") Long foldersId
     ) {
-        return ResponseEntity.ok(pageFoldersUseCase.getOneFoldersMemo(request.getUsersId(), request.getFoldersId()));
+        return ResponseEntity.ok(
+                pageFoldersUseCase.getOneFoldersMemo(usersId, foldersId)
+        );
     }
 }
