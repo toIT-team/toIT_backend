c4dcf3fa16757d5bf7eea300682fb0789ba025e9
whitecy01 <whitecy01@naver.com>
2026-02-17 21:41:13 +0900

    feat(attachments): 필요없는 파일 삭제(AttachMentsImagesCreateRequest)
    Refs: #89

--- src/main/java/com/toit/items/attachments/AttachMentsController.java
+++ src/main/java/com/toit/items/attachments/AttachMentsController.java
@@ -1,18 +1,11 @@
 package com.toit.items.attachments;
 
-import com.toit.items.attachments.dto.request.AttachMentsImagesCreateReqeust;
 import com.toit.items.attachments.dto.response.AttachMentsImagesCreateInFoldersResponse;
-import com.toit.items.links.dto.request.LinksCreateInFoldersRequest;
-import com.toit.items.links.dto.response.LinksCreateInFoldersResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
